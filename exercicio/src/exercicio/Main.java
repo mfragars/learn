80da05cc665fb3a8d78872f0c285df08a582e1f7
Mauricio Fraga de Adnrade <mauricio.fraga@gmail.com>
2017-03-26 20:21:52 -0300

    Fisrt Commit

---
+++ exercicio/src/exercicio/Main.java
@@ -0,0 +1,16 @@
+package exercicio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Lampada lampada = new Lampada();
+		
+		lampada.getStatus();
+		lampada.turnOn();
+		lampada.getStatus();
+		lampada.turnOff();
+		lampada.getStatus();
+
+	}
+
+}
